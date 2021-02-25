cf4172cedd6cab558f2cd4b57cc342ac57942767
Carl <cobettig@gmail.com>
2021-02-25 20:50:03 +0000

    update docs

--- R/tl_provenance.R
+++ R/tl_provenance.R
@@ -1,4 +1,22 @@
-
+#' Provenance information for taxalight data sources
+#' 
+#' `tl_provenance` parses a JSON-LD file describing the data provenance between
+#' the original source data and standardized tables imported by `taxalight` and
+#' `taxadb` R packages.  The JSON-LD markup is specified using the DCAT2 and 
+#' PROV namespaces, which is parsed and returned as a simple data.frame structure.  
+#' 
+#' All content (source data, scripts, and processed tables) are referenced by 
+#' content-based identifiers (see [contentid::content_id()]), which provide automatic
+#' caching of downloads and cryptographic certainty that a version has not been altered.
+#' Rather than list a single download URL as the access point, content identifiers
+#' can be resolves (see [contentid::resolve()]) to any of multiple sources which 
+#' provide the identical content.  This makes them more robust to link rot, much
+#' like a DOI. 
+#' 
+#' @param url The location of the most recent provenance record.  Leave as default.
+#' @return A data frame summarizing the avaialble processed data, along with
+#' identifiers that can be resolved by contentid. 
+#' @export
 tl_provenance <- function(url =
                             paste0("https://raw.githubusercontent.com/",
                                    "boettiger-lab/taxadb-cache/master/prov.json"
